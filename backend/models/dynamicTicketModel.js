e50c8275ad2d7f396b3b9c028587a3f816cf38c5
zeeshan <zeeshanweb.official@gmail.com>
2022-03-05 00:32:30 +0500

    ticket model added

--- backend/models/dynamicTicketModel.js
+++ backend/models/dynamicTicketModel.js
@@ -2,7 +2,35 @@ import mongoose from 'mongoose';
 
 const ticketSchema = new mongoose.Schema(
 {
-
+    user_id:{
+        type:String,
+        required:true
+    },
+    ticket_id:{
+        type:String,
+        required:true
+    },
+    company_name:{
+        type:String,
+        required:true
+    },
+    sum:{
+        type:String,
+        required:true
+    },
+    mega_total:{
+        type:String,
+        required:true
+    },
+    lottery:{
+        type:String,
+        required:true
+    },
+    Data:{
+        type:String,
+        required:true
+    },
+    creation_time: { type: Date, required: true, unique: true,default:Date.now },
 }
 , { strict: false }
 );
